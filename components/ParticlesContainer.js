bbb8f0596b7b462cf296330c8ee5de1f18e7197f
Ivan da Cruz <64236894+ivandacruz@users.noreply.github.com>
2023-09-21 08:52:26 -0300

    add create particles

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -65,9 +65,29 @@ const ParticlesContainer = () => {
             enable: true,
             outModes: {
               default: 'bounce'
-            }
-          }
+            },
+            random: false,
+            speed: 1,
+            straight: false
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800
+            },
+            value: 80
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: 'circle',
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
         },
+        detectRetina: true,
       }} 
     />
   )
